0f96c736d7c441e3ada93dfe70ffd3d45d652c5d
abhijith8201 <83007422+abhijith8201@users.noreply.github.com>
2022-05-17 06:49:57 +0530

    Add files via upload

---
+++ task08/page1/index.js
@@ -0,0 +1,6 @@
+const btn = document.getElementById('btn');
+
+btn.addEventListener('click', function onClick(event) {
+  const box = document.getElementById('box');
+
+  box.style.backgroundColor = 'coral';
